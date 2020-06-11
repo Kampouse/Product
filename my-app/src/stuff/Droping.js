1456cba9cdc1e75d99b203eb69360b4b0e238c85
Jean <jpmartel98@gmail.com>
2020-06-11 15:33:24 -0400

    updated

--- my-app/src/stuff/Droping.js
+++ my-app/src/stuff/Droping.js
@@ -6,27 +6,34 @@ function Droping() {
  
 const { ref, isComponentVisible,setIsComponentVisible } = useComponentVisible("0px");
 const [Maggy, setMaggy] = useState("basic")
-function dog() {
-  setIsComponentVisible("214px")
+  function dog() {
+    // set(max-height  to a high nomber to keep an animation but should be to high to avoid large delay
+  setIsComponentVisible("300px")
   setMaggy("down")
 }
 function cat() {
   setIsComponentVisible("0px")
-  setMaggy("death")
+ 
 } 
 
 
-return(
-<Bigbox  ref={ref}>
-<Drop 
-onClick={() => { 
-  isComponentVisible === "0px"?dog():cat()}}>
-    {Maggy}</Drop>
-       <Dropdown  height = {isComponentVisible}>
-         <DropdownItem>yes</DropdownItem>
-         <DropdownItem>yes</DropdownItem>
-         <DropdownItem>yes</DropdownItem>
-       </Dropdown>
-    </Bigbox>)
+return (
+  <Bigbox ref={ref}>
+    <Drop
+      onClick={() => {
+        isComponentVisible === "0px" ? dog() : cat();
+      }}
+    >
+      {Maggy}
+    </Drop>
+    <Dropdown height={isComponentVisible}>
+      <DropdownItem>yes</DropdownItem>
+      <DropdownItem>yes</DropdownItem>
+      <DropdownItem>yes</DropdownItem>
+      <DropdownItem>yes</DropdownItem>
+      
+    </Dropdown>
+  </Bigbox>
+);
 }
 export default Droping;
--- my-app/src/stuff/style.js
+++ my-app/src/stuff/style.js
@@ -10,12 +10,12 @@ width:25%;
 export const Dropdown = styled.div`
 
 overflow:hidden;
-widht:inherit;
- height:${props => props.height};
+
+max-height:${props => props.height};
 text-align: center;
 cursor:pointer;
 color:palevioletred;
-background-color:palevioletred;
+
 transition:0.5s;
 display:grid;
 border-style:groove;
@@ -23,7 +23,10 @@ border-color:palevioletred;
 border-left-color:palevioletred;   
 border-right-color:palevioletred;
 border-top-color:none;
- transition:1s;
+border-bottom:none;
+border-left:none;
+border-right:none;
+ transition:0.5s;
  &:hover{
 color:purple;
 right:10%;
@@ -32,35 +35,34 @@ transition:1s;
  }
 `;
 
-export const DropdownItem  = styled.a
-`
-position:relative;
-width:122%;
-top:-2px;
-left:-20%;
+export const DropdownItem = styled.a`
+         position: relative;
+         width: auto;
+         top: -2px;
+         left: auto;
 
-padding-left:25%;
-padding-right:25%;
-padding-bottom:25%;
-position:center;
-cursor:pointer;
-background-color:violet;
-color:palevioletred;
-font-size: 1.5em;
-text-align: center;
-border-style:groove;
-border-color:inherit;
-grid-column-gap: 50px;
+        
 
-transition:5s;
-&:hover{
-background-color:pink;
-border-color:pink;
-text-align: center;
-transition:1s;
-color:pink;
-}
-`
+         position: center;
+         cursor: pointer;
+         background-color: violet;
+         color: palevioletred;
+         font-size: 1.5em;
+         text-align: center;
+         border-style: groove;
+         border-top:none;
+         border-width:3px;
+         border-color: palevioletred;
+         grid-column-gap: fit-content;
+
+         transition: 1s;
+         &:hover {
+           background-color: pink;
+           text-align: center;
+           transition: 1s;
+           color: pink;
+         }
+       `;
 
 
 export const Drop  = styled.div`
@@ -72,5 +74,6 @@ border-style:groove;
 color:palevioletred;
 border-color:palevioletred;
 border-bottom:none;
+
 font-size: 1.5em;
 text-align: center;`
