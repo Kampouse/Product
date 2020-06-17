a2d8704620033be2343d568cf0cd1a4dcaa4fe39
Jean <jpmartel98@gmail.com>
2020-06-17 19:21:17 -0400

    apply change

--- my-app/src/stuff/style.js
+++ my-app/src/stuff/style.js
@@ -95,8 +95,8 @@ export const SidePanel = styled.div`
 export const NavBar = styled.div`
 overflow: hidden;
   background-color: hotpink;
-  position: fixed; /* Set the navbar to fixed position */
-  top: 0; /* Position the navbar at the top of the page */
+  position: fixed; 
+  top: 0; 
   width: 100%;
   z-index:1;
   opacity:0.9;
