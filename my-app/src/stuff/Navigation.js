66cdd625bd382c64243e2a0aa769e592380d3edc
Jean <jpmartel98@gmail.com>
2020-06-18 14:23:25 -0400

    Setting global function outside of the component

--- my-app/src/stuff/Navigation.js
+++ my-app/src/stuff/Navigation.js
@@ -4,17 +4,11 @@ import kedge,{useStore} from "kedge"
 import Store from "./Store.js"
 // remove border with vertical line?
 
-
 let Navigation = () => {
-    let PanelSize = useStore( Store.PanelWidth)
- 
-    function SetPanel() {PanelSize === "0px"? Store.PanelWidth.set("200px") : Store.PanelWidth.set("0px")}
- 
+    let PanelSize = useStore( Store.PanelWidth)    
 return(<NavBar>
-
-<NavElement onClick= { () => {SetPanel()}}> left panel </NavElement>
+<NavElement onClick= { () => {Store.SetPanel(Store.PanelWidth,PanelSize)}}> left panel </NavElement>
 <NavElement>long content</NavElement>
-
 <NavElement>content</NavElement>
 <NavElement>content</NavElement>
 <NavElement>content</NavElement>
--- my-app/src/stuff/Store.js
+++ my-app/src/stuff/Store.js
@@ -2,4 +2,7 @@ import { createStore, useStore } from 'kedge';
 
 
  export  default { PanelWidth: createStore("0px"),
-}
+
+ SetPanel : function(PanelWidth,other) 
+ {other ==="0px" ?PanelWidth.set("200px"):PanelWidth.set("0px")}
+ };
